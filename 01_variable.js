bc93d24d1ce3e5b4af054bbc7a59a2b3288ab31e
Trivedi Shivansh <shivanahtrivedi@gmail.com>
2025-02-10 20:23:24 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144230
+let accountEmail = "shivansh@gmail.com"
+var accountPassword = "12345"
+accountcity = "vadodara"
+accountEmail = "shiv@gmail.com"
+accountPassword = "12345"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail.accountPassword,accountcity])
---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert("hello")
+
+console.log(3+3); 
+console.log("shivansh")
+
+let name = "shivansh"
+
+let age = 18
+
+let isLoggedIn = false
+
+//number = 2 to power 53
+//bigint
+// boolean
+//null
+//undefined
+//symbol
+
+
+// oblect
+console.log(typeof null)
